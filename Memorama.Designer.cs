4b853e58bf456442c3e144e7111e44ca9a42be79
Rafael Dorado <Caoz01@live.com.mx>
2024-06-25 23:08:43 -0600

    Little by little

--- Memorama.Designer.cs
+++ Memorama.Designer.cs
@@ -59,10 +59,9 @@
             // 
             // b1
             // 
-            b1.Location = new Point(110, 108);
-            b1.Margin = new Padding(4, 5, 4, 5);
+            b1.Location = new Point(23, 30);
             b1.Name = "b1";
-            b1.Size = new Size(107, 38);
+            b1.Size = new Size(164, 114);
             b1.TabIndex = 0;
             b1.Text = "1";
             b1.UseVisualStyleBackColor = true;
@@ -70,202 +69,201 @@
             // 
             // b2
             // 
-            b2.Location = new Point(251, 108);
-            b2.Margin = new Padding(4, 5, 4, 5);
+            b2.Location = new Point(194, 30);
             b2.Name = "b2";
-            b2.Size = new Size(107, 38);
+            b2.Size = new Size(162, 114);
             b2.TabIndex = 1;
             b2.Text = "2";
             b2.UseVisualStyleBackColor = true;
+            b2.Click += b2_Click;
             // 
             // b3
             // 
-            b3.Location = new Point(393, 108);
-            b3.Margin = new Padding(4, 5, 4, 5);
+            b3.Location = new Point(362, 30);
             b3.Name = "b3";
-            b3.Size = new Size(107, 38);
+            b3.Size = new Size(162, 114);
             b3.TabIndex = 2;
             b3.Text = "3";
             b3.UseVisualStyleBackColor = true;
+            b3.Click += b3_Click;
             // 
             // b4
             // 
-            b4.Location = new Point(536, 108);
-            b4.Margin = new Padding(4, 5, 4, 5);
+            b4.Location = new Point(530, 30);
             b4.Name = "b4";
-            b4.Size = new Size(107, 38);
+            b4.Size = new Size(162, 114);
             b4.TabIndex = 3;
             b4.Text = "4";
             b4.UseVisualStyleBackColor = true;
+            b4.Click += b4_Click;
             // 
             // b5
             // 
-            b5.Location = new Point(681, 108);
-            b5.Margin = new Padding(4, 5, 4, 5);
+            b5.Location = new Point(698, 30);
             b5.Name = "b5";
-            b5.Size = new Size(107, 38);
+            b5.Size = new Size(162, 114);
             b5.TabIndex = 4;
             b5.Text = "5";
             b5.UseVisualStyleBackColor = true;
+            b5.Click += b5_Click;
             // 
             // b10
             // 
-            b10.Location = new Point(681, 180);
-            b10.Margin = new Padding(4, 5, 4, 5);
+            b10.Location = new Point(698, 150);
             b10.Name = "b10";
-            b10.Size = new Size(107, 38);
+            b10.Size = new Size(162, 114);
             b10.TabIndex = 9;
             b10.Text = "10";
             b10.UseVisualStyleBackColor = true;
+            b10.Click += b10_Click;
             // 
             // b9
             // 
-            b9.Location = new Point(536, 180);
-            b9.Margin = new Padding(4, 5, 4, 5);
+            b9.Location = new Point(530, 150);
             b9.Name = "b9";
-            b9.Size = new Size(107, 38);
+            b9.Size = new Size(162, 114);
             b9.TabIndex = 8;
             b9.Text = "9";
             b9.UseVisualStyleBackColor = true;
+            b9.Click += b9_Click;
             // 
             // b8
             // 
-            b8.Location = new Point(393, 180);
-            b8.Margin = new Padding(4, 5, 4, 5);
+            b8.Location = new Point(362, 150);
             b8.Name = "b8";
-            b8.Size = new Size(107, 38);
+            b8.Size = new Size(162, 114);
             b8.TabIndex = 7;
             b8.Text = "8";
             b8.UseVisualStyleBackColor = true;
+            b8.Click += b8_Click;
             // 
             // b7
             // 
-            b7.Location = new Point(251, 180);
-            b7.Margin = new Padding(4, 5, 4, 5);
+            b7.Location = new Point(194, 150);
             b7.Name = "b7";
-            b7.Size = new Size(107, 38);
+            b7.Size = new Size(162, 114);
             b7.TabIndex = 6;
             b7.Text = "7";
             b7.UseVisualStyleBackColor = true;
+            b7.Click += b7_Click;
             // 
             // b6
             // 
-            b6.Location = new Point(110, 180);
-            b6.Margin = new Padding(4, 5, 4, 5);
+            b6.Location = new Point(23, 150);
             b6.Name = "b6";
-            b6.Size = new Size(107, 38);
+            b6.Size = new Size(164, 114);
             b6.TabIndex = 5;
             b6.Text = "6";
             b6.UseVisualStyleBackColor = true;
+            b6.Click += b6_Click;
             // 
             // b15
             // 
-            b15.Location = new Point(681, 247);
-            b15.Margin = new Padding(4, 5, 4, 5);
+            b15.Location = new Point(698, 268);
             b15.Name = "b15";
-            b15.Size = new Size(107, 38);
+            b15.Size = new Size(162, 114);
             b15.TabIndex = 14;
             b15.Text = "15";
             b15.UseVisualStyleBackColor = true;
+            b15.Click += b15_Click;
             // 
             // b14
             // 
-            b14.Location = new Point(536, 247);
-            b14.Margin = new Padding(4, 5, 4, 5);
+            b14.Location = new Point(530, 268);
             b14.Name = "b14";
-            b14.Size = new Size(107, 38);
+            b14.Size = new Size(162, 114);
             b14.TabIndex = 13;
             b14.Text = "14";
             b14.UseVisualStyleBackColor = true;
+            b14.Click += b14_Click;
             // 
             // b13
             // 
-            b13.Location = new Point(393, 247);
-            b13.Margin = new Padding(4, 5, 4, 5);
+            b13.Location = new Point(362, 268);
             b13.Name = "b13";
-            b13.Size = new Size(107, 38);
+            b13.Size = new Size(162, 114);
             b13.TabIndex = 12;
             b13.Text = "13";
             b13.UseVisualStyleBackColor = true;
+            b13.Click += b13_Click;
             // 
             // b12
             // 
-            b12.Location = new Point(251, 247);
-            b12.Margin = new Padding(4, 5, 4, 5);
+            b12.Location = new Point(194, 268);
             b12.Name = "b12";
-            b12.Size = new Size(107, 38);
+            b12.Size = new Size(162, 114);
             b12.TabIndex = 11;
             b12.Text = "12";
             b12.UseVisualStyleBackColor = true;
+            b12.Click += b12_Click;
             // 
             // b11
             // 
-            b11.Location = new Point(110, 247);
-            b11.Margin = new Padding(4, 5, 4, 5);
+            b11.Location = new Point(23, 268);
             b11.Name = "b11";
-            b11.Size = new Size(107, 38);
+            b11.Size = new Size(164, 114);
             b11.TabIndex = 10;
             b11.Text = "11";
             b11.UseVisualStyleBackColor = true;
+            b11.Click += b11_Click;
             // 
             // b20
             // 
-            b20.Location = new Point(681, 323);
-            b20.Margin = new Padding(4, 5, 4, 5);
+            b20.Location = new Point(698, 388);
             b20.Name = "b20";
-            b20.Size = new Size(107, 38);
+            b20.Size = new Size(162, 114);
             b20.TabIndex = 19;
             b20.Text = "20";
             b20.UseVisualStyleBackColor = true;
+            b20.Click += b20_Click;
             // 
             // b19
             // 
-            b19.Location = new Point(536, 323);
-            b19.Margin = new Padding(4, 5, 4, 5);
+            b19.Location = new Point(530, 388);
             b19.Name = "b19";
-            b19.Size = new Size(107, 38);
+            b19.Size = new Size(162, 114);
             b19.TabIndex = 18;
             b19.Text = "19";
             b19.UseVisualStyleBackColor = true;
+            b19.Click += b19_Click;
             // 
             // b18
             // 
-            b18.Location = new Point(393, 323);
-            b18.Margin = new Padding(4, 5, 4, 5);
+            b18.Location = new Point(362, 388);
             b18.Name = "b18";
-            b18.Size = new Size(107, 38);
+            b18.Size = new Size(162, 114);
             b18.TabIndex = 17;
             b18.Text = "18";
             b18.UseVisualStyleBackColor = true;
+            b18.Click += b18_Click;
             // 
             // b17
             // 
-            b17.Location = new Point(251, 323);
-            b17.Margin = new Padding(4, 5, 4, 5);
+            b17.Location = new Point(194, 388);
             b17.Name = "b17";
-            b17.Size = new Size(107, 38);
+            b17.Size = new Size(162, 114);
             b17.TabIndex = 16;
             b17.Text = "17";
             b17.UseVisualStyleBackColor = true;
+            b17.Click += b17_Click;
             // 
             // b16
             // 
-            b16.Location = new Point(110, 323);
-            b16.Margin = new Padding(4, 5, 4, 5);
+            b16.Location = new Point(23, 388);
             b16.Name = "b16";
-            b16.Size = new Size(107, 38);
+            b16.Size = new Size(164, 114);
             b16.TabIndex = 15;
             b16.Text = "16";
             b16.UseVisualStyleBackColor = true;
+            b16.Click += b16_Click;
             // 
             // anuncioMovimiento
             // 
             anuncioMovimiento.AutoSize = true;
             anuncioMovimiento.Font = new Font("Segoe UI", 12F);
-            anuncioMovimiento.Location = new Point(1001, 108);
-            anuncioMovimiento.Margin = new Padding(4, 0, 4, 0);
+            anuncioMovimiento.Location = new Point(912, 45);
             anuncioMovimiento.Name = "anuncioMovimiento";
-            anuncioMovimiento.Size = new Size(144, 32);
+            anuncioMovimiento.Size = new Size(94, 21);
             anuncioMovimiento.TabIndex = 20;
             anuncioMovimiento.Text = "Movimiento";
             // 
@@ -273,10 +271,9 @@
             // 
             conteoMovimiento.AutoSize = true;
             conteoMovimiento.Font = new Font("Segoe UI", 18F);
-            conteoMovimiento.Location = new Point(1049, 165);
-            conteoMovimiento.Margin = new Padding(4, 0, 4, 0);
+            conteoMovimiento.Location = new Point(945, 79);
             conteoMovimiento.Name = "conteoMovimiento";
-            conteoMovimiento.Size = new Size(39, 48);
+            conteoMovimiento.Size = new Size(27, 32);
             conteoMovimiento.TabIndex = 21;
             conteoMovimiento.Text = "0";
             conteoMovimiento.TextAlign = ContentAlignment.MiddleCenter;
@@ -285,10 +282,9 @@
             // 
             anuncioTiempo.AutoSize = true;
             anuncioTiempo.Font = new Font("Segoe UI", 12F);
-            anuncioTiempo.Location = new Point(1020, 270);
-            anuncioTiempo.Margin = new Padding(4, 0, 4, 0);
+            anuncioTiempo.Location = new Point(925, 142);
             anuncioTiempo.Name = "anuncioTiempo";
-            anuncioTiempo.Size = new Size(95, 32);
+            anuncioTiempo.Size = new Size(62, 21);
             anuncioTiempo.TabIndex = 22;
             anuncioTiempo.Text = "Tiempo";
             // 
@@ -296,29 +292,26 @@
             // 
             contadorTiempo.AutoSize = true;
             contadorTiempo.Font = new Font("Segoe UI", 18F);
-            contadorTiempo.Location = new Point(1014, 323);
-            contadorTiempo.Margin = new Padding(4, 0, 4, 0);
+            contadorTiempo.Location = new Point(921, 174);
             contadorTiempo.Name = "contadorTiempo";
-            contadorTiempo.Size = new Size(104, 48);
+            contadorTiempo.Size = new Size(71, 32);
             contadorTiempo.TabIndex = 23;
             contadorTiempo.Text = "00:00";
             // 
             // botonPausar
             // 
-            botonPausar.Location = new Point(1014, 435);
-            botonPausar.Margin = new Padding(4, 5, 4, 5);
+            botonPausar.Location = new Point(921, 241);
             botonPausar.Name = "botonPausar";
-            botonPausar.Size = new Size(107, 38);
+            botonPausar.Size = new Size(75, 23);
             botonPausar.TabIndex = 24;
             botonPausar.Text = "Pausar";
             botonPausar.UseVisualStyleBackColor = true;
             // 
             // botonCancelar
             // 
-            botonCancelar.Location = new Point(536, 540);
-            botonCancelar.Margin = new Padding(4, 5, 4, 5);
+            botonCancelar.Location = new Point(912, 334);
             botonCancelar.Name = "botonCancelar";
-            botonCancelar.Size = new Size(107, 38);
+            botonCancelar.Size = new Size(94, 37);
             botonCancelar.TabIndex = 25;
             botonCancelar.Text = "Cancelar";
             botonCancelar.UseVisualStyleBackColor = true;
@@ -326,10 +319,9 @@
             // 
             // testing
             // 
-            testing.Location = new Point(661, 435);
-            testing.Margin = new Padding(4, 5, 4, 5);
+            testing.Location = new Point(921, 445);
             testing.Name = "testing";
-            testing.Size = new Size(107, 38);
+            testing.Size = new Size(75, 23);
             testing.TabIndex = 26;
             testing.Text = "test";
             testing.UseVisualStyleBackColor = true;
@@ -337,9 +329,9 @@
             // 
             // Memorama
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1257, 750);
+            ClientSize = new Size(1076, 532);
             Controls.Add(testing);
             Controls.Add(botonCancelar);
             Controls.Add(botonPausar);
@@ -367,9 +359,10 @@
             Controls.Add(b3);
             Controls.Add(b2);
             Controls.Add(b1);
-            Margin = new Padding(4, 5, 4, 5);
             Name = "Memorama";
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "Memorama";
+            Load += Memorama_Load;
             ResumeLayout(false);
             PerformLayout();
         }
--- Memorama.cs
+++ Memorama.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Memorama.Properties;
+using Microsoft.VisualBasic.ApplicationServices;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -30,14 +32,193 @@ namespace Memorama
 
         private void b1_Click(object sender, EventArgs e)
         {
-            b1.Image = Image.FromFile("D:\\Codigos\\Memorama\\Memorama\\imagenes\\t1.jpg");
+            b1.Text =null;
+            b1.Image = Image.FromFile("C:\\Users\\Caoz0\\source\\repos\\Memorama\\Resources\\Risu.jpg");
             b1.AutoSize = true;
         }
 
         private void testing_Click(object sender, EventArgs e)
         {
             b1.Image = null;
+            b2.Image = null;
+            b3.Image = null;
+            b4.Image = null;
+            b5.Image = null;
+            b6.Image = null;
+            b7.Image = null;
+            b8.Image = null;
+            b9.Image = null;
+            b10.Image = null;
+            b11.Image = null;
+            b12.Image = null;
+            b13.Image = null;
+            b14.Image = null;
+            b15.Image = null;
+            b16.Image = null;
+            b17.Image = null;
+            b18.Image = null;
+            b19.Image = null;
+            b20.Image = null;
+
             b1.AutoSize = false;
+            b2.AutoSize = false;
+            b3.AutoSize = false;
+            b4.AutoSize = false;
+            b5.AutoSize = false;
+            b6.AutoSize = false;
+            b7.AutoSize = false;
+            b8.AutoSize = false;
+            b9.AutoSize = false;
+            b10.AutoSize = false;
+            b11.AutoSize = false;
+            b12.AutoSize = false;
+            b13.AutoSize = false;
+            b14.AutoSize = false;
+            b15.AutoSize = false;
+            b16.AutoSize = false;
+            b17.AutoSize = false;
+            b18.AutoSize = false;
+            b19.AutoSize = false;
+            b20.AutoSize = false;
+
+        }
+
+        private void Memorama_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void b15_Click(object sender, EventArgs e)
+        {
+            b15.Text =null;
+            b15.Image = Image.FromFile("C:\\Users\\Caoz0\\source\\repos\\Memorama\\Resources\\Bae.jpg");
+            b15.AutoSize = true;
+        }
+
+        private void b10_Click(object sender, EventArgs e)
+        {
+            b10.Text = null;
+            b10.Image = Image.FromFile("C:\\Users\\Caoz0\\source\\repos\\Memorama\\Resources\\Sora.jpg");
+            b10.AutoSize = true;
+        }
+
+        private void b11_Click(object sender, EventArgs e)
+        {
+            b11.Text = null;
+            b11.Image = Image.FromFile("C:\\Users\\Caoz0\\source\\repos\\Memorama\\Resources\\Aki.jpg");
+            b11.AutoSize = true;
+        }
+
+        private void b5_Click(object sender, EventArgs e)
+        {
+            b5.Text = null;
+            b5.Image = Image.FromFile("C:\\Users\\Caoz0\\source\\repos\\Memorama\\Resources\\Aki.jpg");
+            b5.AutoSize = true;
+        }
+
+        private void b13_Click(object sender, EventArgs e)
+        {
+            b13.Text = null;
+            b13.Image = Image.FromFile("C:\\Users\\Caoz0\\source\\repos\\Memorama\\Resources\\AZKi.jpg");
+            b13.AutoSize = true;
+        }
+
+        private void b12_Click(object sender, EventArgs e)
+        {
+            b12.Text = null;
+            b12.Image = Image.FromFile("C:\\Users\\Caoz0\\source\\repos\\Memorama\\Resources\\AZKi.jpg");
+            b12.AutoSize = true;
+        }
+
+        private void b2_Click(object sender, EventArgs e)
+        {
+            b2.Text = null;
+            b2.Image = Image.FromFile("C:\\Users\\Caoz0\\source\\repos\\Memorama\\Resources\\Bae.jpg");
+            b2.AutoSize = true;
+        }
+
+        private void b4_Click(object sender, EventArgs e)
+        {
+            b4.Text = null;
+            b4.Image = Image.FromFile("C:\\Users\\Caoz0\\source\\repos\\Memorama\\Resources\\Calli.jpg");
+            b4.AutoSize = true;
+        }
+
+        private void b6_Click(object sender, EventArgs e)
+        {
+            b6.Text = null;
+            b6.Image = Image.FromFile("C:\\Users\\Caoz0\\source\\repos\\Memorama\\Resources\\Calli.jpg");
+            b6.AutoSize = true;
+        }
+
+        private void b8_Click(object sender, EventArgs e)
+        {
+            b8.Text = null;
+            b8.Image = Image.FromFile("C:\\Users\\Caoz0\\source\\repos\\Memorama\\Resources\\IRyS.jpg");
+            b8.AutoSize = true;
+        }
+
+        private void b16_Click(object sender, EventArgs e)
+        {
+            b16.Text = null;
+            b16.Image = Image.FromFile("C:\\Users\\Caoz0\\source\\repos\\Memorama\\Resources\\IRyS.jpg");
+            b16.AutoSize = true;
+        }
+
+        private void b19_Click(object sender, EventArgs e)
+        {
+            b19.Text = null;
+            b19.Image = Image.FromFile("C:\\Users\\Caoz0\\source\\repos\\Memorama\\Resources\\Kiara.jpg");
+            b19.AutoSize = true;
+        }
+
+        private void b7_Click(object sender, EventArgs e)
+        {
+            b7.Text = null;
+            b7.Image = Image.FromFile("C:\\Users\\Caoz0\\source\\repos\\Memorama\\Resources\\Kiara.jpg");
+            b7.AutoSize = true;
+        }
+
+        private void b18_Click(object sender, EventArgs e)
+        {
+            b18.Text = null;
+            b18.Image = Image.FromFile("C:\\Users\\Caoz0\\source\\repos\\Memorama\\Resources\\Korone.jpg");
+            b18.AutoSize = true;
+        }
+
+        private void b17_Click(object sender, EventArgs e)
+        {
+            b17.Text = null;
+            b17.Image = Image.FromFile("C:\\Users\\Caoz0\\source\\repos\\Memorama\\Resources\\Korone.jpg");
+            b17.AutoSize = true;
+        }
+
+        private void b14_Click(object sender, EventArgs e)
+        {
+            b14.Text = null;
+            b14.Image = Image.FromFile("C:\\Users\\Caoz0\\source\\repos\\Memorama\\Resources\\Ollie.jpg");
+            b14.AutoSize = true;
+        }
+
+        private void b3_Click(object sender, EventArgs e)
+        {
+            b3.Text = null;
+            b3.Image = Image.FromFile("C:\\Users\\Caoz0\\source\\repos\\Memorama\\Resources\\Ollie.jpg");
+            b3.AutoSize = true;
+        }
+
+        private void b20_Click(object sender, EventArgs e)
+        {
+            b20.Text = null;
+            b20.Image = Image.FromFile("C:\\Users\\Caoz0\\source\\repos\\Memorama\\Resources\\Risu.jpg");
+            b20.AutoSize = true;
+        }
+
+        private void b9_Click(object sender, EventArgs e)
+        {
+            b9.Text = null;
+            b9.Image = Image.FromFile("C:\\Users\\Caoz0\\source\\repos\\Memorama\\Resources\\Sora.jpg");
+            b9.AutoSize = true;
         }
     }
 }
