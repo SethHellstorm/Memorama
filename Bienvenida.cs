a1ce383d49338aee7e4bc533a060c79575c8bb1e
Rafael Dorado <Caoz01@live.com.mx>
2024-07-01 19:04:47 -0600

    Comments correction

--- Bienvenida.cs
+++ Bienvenida.cs
@@ -13,52 +13,50 @@ namespace Memorama
     public partial class Bienvenida : Form
     {
         Registro regis; //Crea la instancia de la siguiente ventana
-        string[,] memoriaInicial = new string[5, 5]; //Instaciamos e inicializamos el arreglo de 5 por 5 el cual contendra los puntuajes mas altos.
-
+        string[,] memoriaInicial = new string[5, 5]; //Instanciamos e inicializamos el arreglo de 5 por 5 el cual contendrá los puntajes más altos.
         public Bienvenida()
         {
             InitializeComponent(); // Inicializamos los componentes de la ventana
         }
-
         private void bIniciar_Click(object sender, EventArgs e)
         { //Funcion de lcik para el boton de iniciar
-            memoriaInicial[0, 0] = "Rafael"; //Rellenamos el arreglo con la informacion base, damos nombre
+            memoriaInicial[0, 0] = "Rafael"; //Rellenamos el arreglo con la información base, damos nombre
             memoriaInicial[0, 1] = "Seth"; // Damos nickname
             memoriaInicial[0, 2] = "29"; // Damos edad
             memoriaInicial[0, 3] = "85"; //Damos movimientos
-            memoriaInicial[0, 4] = "00:15:31"; // Damos tiempo en el formate Horas:Minutos:Segundos
+            memoriaInicial[0, 4] = "00:15:31"; // Damos tiempo en el formato Horas:Minutos:Segundos
 
             memoriaInicial[1, 0] = ""; // Damos nombre
             memoriaInicial[1, 1] = ""; // Damos nickname
             memoriaInicial[1, 2] = ""; // Damos edad
             memoriaInicial[1, 3] = ""; //Damos movimientos
-            memoriaInicial[1, 4] = "22:58:58"; // Damos tiempo en el formate Horas:Minutos:Segundos
+            memoriaInicial[1, 4] = "22:58:58"; // Damos tiempo en el formato Horas:Minutos:Segundos
 
             memoriaInicial[2, 0] = ""; // Damos nombre
             memoriaInicial[2, 1] = ""; // Damos nickname
             memoriaInicial[2, 2] = ""; // Damos edad
             memoriaInicial[2, 3] = ""; //Damos movimientos
-            memoriaInicial[2, 4] = "22:58:58"; // Damos tiempo en el formate Horas:Minutos:Segundos
+            memoriaInicial[2, 4] = "22:58:58"; // Damos tiempo en el formato Horas:Minutos:Segundos
 
             memoriaInicial[3, 0] = ""; // Damos nombre
             memoriaInicial[3, 1] = ""; // Damos nickname
             memoriaInicial[3, 2] = ""; // Damos edad
             memoriaInicial[3, 3] = ""; //Damos movimientos
-            memoriaInicial[3, 4] = "22:58:58"; // Damos tiempo en el formate Horas:Minutos:Segundos
+            memoriaInicial[3, 4] = "22:58:58"; // Damos tiempo en el formato Horas:Minutos:Segundos
 
             memoriaInicial[4, 0] = ""; // Damos nombre
             memoriaInicial[4, 1] = ""; // Damos nickname
             memoriaInicial[4, 2] = ""; // Damos edad
             memoriaInicial[4, 3] = ""; //Damos movimientos
-            memoriaInicial[4, 4] = "22:58:58"; // Damos tiempo en el formate Horas:Minutos:Segundos
+            memoriaInicial[4, 4] = "22:58:58"; // Damos tiempo en el formato Horas:Minutos:Segundos
 
             regis = new Registro(memoriaInicial); //Inicializamos la ventana siguiente y ke damos como argumenta el arreglo de memoria
             regis.Show(); //Mostramos la siguiente ventana
             this.Hide(); //Escondemos esta
         }
-
         private void Bienvenida_Load(object sender, EventArgs e)
         {
         }
     }
 }
+
--- Registro.cs
+++ Registro.cs
@@ -5,23 +5,23 @@ namespace Memorama
 {
     public partial class Registro : Form
     {
-        private static string[] info = new string[3];
-        string[,] memo;
-        Memorama mem;
+        private static string[] info = new string[3]; //Instanciamos e inicializamos el arreglo info el cual guardara nombre, nickname y edad del participante
+        string[,] memo; //Instanciamos el arreglo memo para en este guardar lo que se nos pase de memoria
+        Memorama mem; //Instanciamos la siguiente ventana
         public Registro(string[,] memoria)
-        {
-            InitializeComponent();
-            memo = memoria;
+        {//Recivimos como argumento el arreglo multidimensional de memoria
+            InitializeComponent(); // Inicializamos los componentes
+            memo = memoria; // Pasamos lo que recibimos al arreglo local
         }
 
         private void button1_Click(object sender, EventArgs e)
-        {
-            Application.Exit();
+        {// Evento click para el boton salir
+            Application.Exit(); // Damos para que la aplicacion se cierre completamente
         }
 
         private void botonIniciar_Click(object sender, EventArgs e)
-        {
-            info[0] = textoNombre.Text;
+        {// Evento click para el boton inciar
+            info[0] = textoNombre.Text; //
             info[1] = textoNickname.Text;
             info[2] = textoEdad.Text;
             mem = new Memorama(this, memo,info);
