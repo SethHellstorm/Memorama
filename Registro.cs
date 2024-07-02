64f006389241931386c2f8f610c9b69300ab540f
Rafael Dorado <Caoz01@live.com.mx>
2024-07-01 19:07:57 -0600

    More comments

--- Registro.cs
+++ Registro.cs
@@ -5,34 +5,30 @@ namespace Memorama
 {
     public partial class Registro : Form
     {
-        private static string[] info = new string[3]; //Instanciamos e inicializamos el arreglo info el cual guardara nombre, nickname y edad del participante
+        private static string[] info = new string[3]; //Instanciamos e inicializamos el arreglo info el cual guardará nombre, nickname y edad del participante
         string[,] memo; //Instanciamos el arreglo memo para en este guardar lo que se nos pase de memoria
         Memorama mem; //Instanciamos la siguiente ventana
         public Registro(string[,] memoria)
-        {//Recivimos como argumento el arreglo multidimensional de memoria
+        {//Recibimos como argumento el arreglo multidimensional de memoria
             InitializeComponent(); // Inicializamos los componentes
             memo = memoria; // Pasamos lo que recibimos al arreglo local
         }
-
         private void button1_Click(object sender, EventArgs e)
-        {// Evento click para el boton salir
-            Application.Exit(); // Damos para que la aplicacion se cierre completamente
+        {// Evento click para el botón salir
+            Application.Exit(); // Damos para que la aplicación se cierre completamente
         }
-
         private void botonIniciar_Click(object sender, EventArgs e)
-        {// Evento click para el boton inciar
-            info[0] = textoNombre.Text; //
-            info[1] = textoNickname.Text;
-            info[2] = textoEdad.Text;
-            mem = new Memorama(this, memo,info);
-            mem.Show();
-            this.Hide();
-
+        {// Evento click para el boton iniciar
+            info[0] = textoNombre.Text; //Capturamos el nombre al arreglo
+            info[1] = textoNickname.Text;//Capturamos el nickname al arreglo
+            info[2] = textoEdad.Text;//Capturamos la edad al arreglo
+            mem = new Memorama(this, memo, info);//Inicializamos la siguiente ventana y pasamos como argumento, la instancia de esta ventana, la memoria y la información capturada
+            mem.Show();// Mostramos la siguiente ventana
+            this.Hide();// Ocultamos esta ventana
         }
-        
-
         private void Registro_Load(object sender, EventArgs e)
         {
         }
     }
 }
+
